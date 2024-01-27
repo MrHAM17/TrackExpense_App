b0f7ec23e255abb2efc5738aefe75c7dedebf598
MrHAM17 <harshminde1@gmail.com>
2024-01-27 15:57:23 +0530

    Its a Trial 01 - Level 01
    27th Jan 2024
    
    Direct from reference code/repo.
    
    Build on my system.
    Completely working, no errors.
    Just need to change & update it overall.

---
+++ T1_Level 01/TrackExpense/app/src/main/java/com/example/trackexpense/views/activites/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.trackexpense.views.activites;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.trackexpense.R;
+import com.example.trackexpense.databinding.ActivityMainBinding;
+import com.example.trackexpense.utils.Constants;
+import com.example.trackexpense.viewmodels.MainViewModel;
+import com.example.trackexpense.views.fragments.StatsFragment;
+import com.example.trackexpense.views.fragments.TransactionsFragment;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    Calendar calendar;
+    /*
+    0 = Daily
+    1 = Monthly
+    2 = Calendar
+    3 = Summary
+    4 = Notes
+     */
+
+
+    public MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+
+
+        setSupportActionBar(binding.toolBar);
+        getSupportActionBar().setTitle("Transactions");
+
+
+        Constants.setCategories();
+
+        calendar = Calendar.getInstance();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, new TransactionsFragment());
+        transaction.commit();
+
+        binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                if(item.getItemId() == R.id.transactions) {
+                    getSupportFragmentManager().popBackStack();
+                } else if(item.getItemId() == R.id.stats){
+                    transaction.replace(R.id.content, new StatsFragment());
+                    transaction.addToBackStack(null);
+                }
+                transaction.commit();
+                return true;
+            }
+        });
+
+
+    }
+
+    public void getTransactions() {
+        viewModel.getTransactions(calendar);
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
